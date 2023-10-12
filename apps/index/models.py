17a2c820b19f18f6ae11c136d45c0899d294b3cf
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-10-12 19:00:26 -0400

    update settings/core

--- apps/index/models.py
+++ apps/index/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django_resized.forms import ResizedImageField 
 
 # Create your models here.
+class Settings(models.Model):
+            verbose_name_plural = "Основные настройки"
+      
