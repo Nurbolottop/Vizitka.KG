12612d787307424b4756014e7a4b38ce071e2f95
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-10-17 11:24:03 -0400

    update advert

--- apps/index/views.py
+++ apps/index/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-
+import requests
 # my imports
 from apps.index import models
 from apps.blog.models import Blog
@@ -17,6 +17,16 @@ def index(request):
     popular_posts = Blog.objects.order_by('-views')[:3]
     popular_post = Blog.objects.order_by('-views')[:1]
     big_advert = BigAdvert.objects.latest('id')
+    api_url = "http://www.cbr.ru/scripts/XML_daily.asp"
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        # Проверяем наличие ключа 'rates' в ответе
+        if 'rates' in data:
+            usd_rate = data['rates']['USD']
+            rub_rate = data['rates']['RUB']
+            eur_rate = data['rates']['EUR']
     
     return render(request, 'base/home-default.html', locals())
 
