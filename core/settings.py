51f4814206d95e397862923e1b1cefa7cacdf582
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-11-30 01:21:23 -0500

    update

--- core/settings.py
+++ core/settings.py
@@ -20,10 +20,13 @@ import config as config
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.SECRET_KEY
+# SECRET_KEY = config.SECRET_KEY
+SECRET_KEY = "django-insecure-so%q9=7d0zk^m^z35pll#v@)ak0&f=-r3r0o^1iu$#b(ki4w#c"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config.DEBUG
+DEBUG = True
+
 
 ALLOWED_HOSTS = ['*']
 
