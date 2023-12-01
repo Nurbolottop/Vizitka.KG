3b9ab4238e23046d688ad7587c8618c938223b05
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-11-30 19:50:58 -0500

    Up

---
+++ config.py
@@ -0,0 +1,11 @@
+SECRET_KEY = 'django-insecure-so%q9=7d0zk^m^z35pll#v@)ak0&f=-r3r0o^1iu$#b(ki4w#c'
+DEBUG = True
+
+LANGUAGE_CODE = 'ru'
+
+TIME_ZONE = 'Asia/Bishkek'
+
+USE_I18N = True
+
+USE_TZ = True
+TELEGRAM_BOT_TOKEN = '6583749734:AAG3PM0QImo--VOBLd2eSyBf0X4Pcr--K9c'
